bec46c273a9c6216c4a547bb22a00d7102545fc3
zhuyuan <849434163@qq.com>
2017-03-06 19:37:49 +0800

    bugfix_master_hushengyong_add_myrabbitmq.lua

---
+++ myrabbitmq.lua
@@ -0,0 +1,16 @@
+#!/usr/bin/env lua
+
+local rabbitmq_c = require "rabbitmqstomp"
+
+local _M = { _VERSION = "0.1" }
+local mt = { __index = _M }
+
+
+function _M:new(opts, addr, timeout)
+    opts = opts or {username = "guest", password = "guest", vhost = "/", trailing_lf = false}
+    addr = addr or {host = "127.0.0.1", port = 61613}
+    timeout = timeout or 10000
+    return setmetatable({ opts = opts, addr = addr, timeout = timeout }, mt)
+
+
+return _M
