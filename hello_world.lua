708a082ab351ddfc341bb7242cfa7b36d6e06e59
zhuyuan <849434163@qq.com>
2017-03-06 19:23:39 +0800

    bugfix_master_hushengyong_init

---
+++ hello_world.lua
@@ -0,0 +1,3 @@
+#!/usr/bin/env lua
+
+ngx.say("hello_world")
---
+++ test_redis.lua
@@ -0,0 +1,14 @@
+#!/usr/bin/env lua
+
+local redis = require "redis_iresty"
+local red = redis:new()
+
+print("########## connect redis success ##########")
+local ok, err = red:auth("7712572")
+if not ok then ngx.say("failed to auth:", err) return end
+print("########## auth redis success ##########")
+
+local ok, err = red:set("dog", "an animal")
+if not ok then ngx.say("failed to set dog:", err) return end
+
+ngx.say("set result: ", ok)
