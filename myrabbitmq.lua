32ad8c6a94d6c3307085acaa63a41ceeeca0c477
zhuyuan <849434163@qq.com>
2017-03-06 19:48:36 +0800

    bugfix_pre_master_hushengyong_update_myrabbitmq.lua

--- myrabbitmq.lua
+++ myrabbitmq.lua
@@ -6,6 +6,25 @@ local _M = { _VERSION = "0.1" }
 local mt = { __index = _M }
 
 
+function _M:send(msg, headers)
+    local mq, err = rabbitmq_c:new(self.opts)
+    if not mq or err then return nil, err end
+    
+    mq:set_timeout(self.timeout)
+    
+    local ok, err = mq:connect(self.addr.host, self.addr.port)
+    if not ok or err then return nil, err end
+
+    local result, err = mq:send(msg, headers)
+    if not ok or err then return nil, err end
+
+    local ok, err = mq:set_keepalive(60000, 1000)
+    if not ok or err then return nil, err end
+
+    return result, err
+end
+
+
 function _M:new(opts, addr, timeout)
     opts = opts or {username = "guest", password = "guest", vhost = "/", trailing_lf = false}
     addr = addr or {host = "127.0.0.1", port = 61613}
